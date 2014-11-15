edace767eb86d6d866525a325c5286d90f7080b8
patkovskyi <patkovskyi@gmail.com>
2014-11-15 00:13:00 -0800

    First test passed

--- test/jstore/test/TrieTest.java
+++ test/jstore/test/TrieTest.java
@@ -1,19 +1,19 @@
 package jstore.test;
 
-import jstore.StringSet;
+import jstore.Trie;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TrieTest {
 
-	@Test
-	public void test() {
-		String[] data = { "a", "aa", "ab", "abc" };		
-		StringSet set = StringSet.create(data);
-		String[] actual = set.getStrings();
-		
-		Assert.assertArrayEquals("oops", data, actual);
-	}
+  @Test
+  public void test() {
+    String[] data = {"a", "aa", "ab", "abc"};
+    Trie set = Trie.create(data);
+    String[] actual = set.getStrings();
+
+    Assert.assertArrayEquals("oops", data, actual);
+  }
 
 }
