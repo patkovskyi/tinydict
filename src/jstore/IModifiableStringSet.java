9fc2c3f86378e935fd78827c6d55434793b2bb9c
patkovskyi <patkovskyi@gmail.com>
2014-11-18 23:53:13 -0800

    Test works under maven from eclipse

--- src/jstore/IModifiableStringSet.java
+++
@@ -1,7 +0,0 @@
-package jstore;
-
-public interface IModifiableStringSet extends IStringSet {
-	public void add(String string);
-
-	public boolean remove(String string);
-}
--- src/jstore/IStringSet.java
+++
@@ -1,7 +0,0 @@
-package jstore;
-
-public interface IStringSet {
-	public boolean contains(String string);
-	
-	public String[] getStrings(); 
-}
