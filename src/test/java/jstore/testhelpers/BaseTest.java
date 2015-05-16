7d3f4ea153a171e4f25f4eb0c4f6c1759bbd68b4
patkovskyi <patkovskyi@gmail.com>
2015-05-16 00:14:35 -0700

    Ability to configurate which StringSetFactories to test

--- src/test/java/jstore/testhelpers/BaseTest.java
+++ src/test/java/jstore/testhelpers/BaseTest.java
@@ -1,18 +1,40 @@
 package jstore.testhelpers;
 
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
+
 import jstore.testhelpers.rivals.StringSetFactory;
 import jstore.testhelpers.rivals.arrayset.ArrayStringSetFactory;
 import jstore.testhelpers.rivals.hashset.HashStringSetFactory;
 import jstore.testhelpers.rivals.mafsa.BinaryMafsaStringSetFactory;
 import jstore.testhelpers.rivals.mafsa.LinearMafsaStringSetFactory;
 
-import org.testng.annotations.DataProvider;
-
 public class BaseTest {
   @DataProvider(name = "factories", parallel = true)
-  public StringSetFactory[][] getFactories() {
-    return new StringSetFactory[][] { {new LinearMafsaStringSetFactory()},
-        {new BinaryMafsaStringSetFactory()}, {new ArrayStringSetFactory()},
-        {new HashStringSetFactory()}};
+  public StringSetFactory[][] getFactories(ITestContext context) {
+
+    String factoryClassesParam = context.getCurrentXmlTest().getParameter("factoryClasses");
+    if (factoryClassesParam == null) {
+      return new StringSetFactory[][]{{new LinearMafsaStringSetFactory()},
+                                      {new BinaryMafsaStringSetFactory()},
+                                      {new ArrayStringSetFactory()},
+                                      {new HashStringSetFactory()}};
+    }
+
+    String[] factoryClasses = factoryClassesParam.split(",");
+    StringSetFactory[][] factories = new StringSetFactory[factoryClasses.length][0];
+    for (int i = 0; i < factoryClasses.length; i++) {
+      try {
+        factories[i] =
+            new StringSetFactory[]{
+                (StringSetFactory) Class.forName(factoryClasses[i].trim()).newInstance()};
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
+        System.err.println(
+            "Make sure that you have fully-specified and correct factoryClasses in testng*.xml");
+      }
+    }
+
+    return  factories;
   }
 }
