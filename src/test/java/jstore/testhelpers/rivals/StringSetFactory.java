f244f8791dcddc2ffd00366eae94229a5fc9d5ae
patkovskyi <patkovskyi@gmail.com>
2015-08-02 21:12:27 -0700

    Removed serialization logic from StringSetFactory

--- src/test/java/jstore/testhelpers/rivals/StringSetFactory.java
+++ src/test/java/jstore/testhelpers/rivals/StringSetFactory.java
@@ -1,23 +1,12 @@
 package jstore.testhelpers.rivals;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 
 import jstore.Messages;
-import jstore.Serializer;
 import jstore.StringSet;
 
 public abstract class StringSetFactory {
-  public StringSet deserialize(String path) throws ClassNotFoundException, IOException {
-    return Serializer.deserialize(path);
-  }
-
-  public void serialize(StringSet stringSet, String path) throws ClassNotFoundException,
-      IOException {
-    Serializer.serialize(stringSet, path);
-  }
-
   public StringSet create(String[] strings) {
     if (strings == null) {
       throw new IllegalArgumentException(Messages.STRING_COLLECTION_CAN_NOT_BE_NULL);
